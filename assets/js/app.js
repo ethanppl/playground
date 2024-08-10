71dc4270425fee7d7ecd692fe4f57b76e10f7f35
Ethan <43922849+ethanppl@users.noreply.github.com>
2024-08-10 17:47:33 +0800

    Small improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,6 +40,8 @@ let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
+  timeout: 5000,
+  loaderTimeout: 1000,
   hooks: Hooks
 })
 
