c1a276e29dd431c6b7279a128bfcc3653e7bc975
Ethan <43922849+ethanppl@users.noreply.github.com>
2024-08-24 23:06:22 +0800

    Show how to play in info modal

--- lib/playground_web/components/games_components/super_hangman/how_to_play.ex
+++ lib/playground_web/components/games_components/super_hangman/how_to_play.ex
@@ -33,13 +33,19 @@ defmodule PlaygroundWeb.GamesComponents.SuperHangmanComponent.HowToPlay do
       <div class="p-2">
         <img src="/images/games/super-hangman.png" class="w-11/12 mx-auto" />
         <div class="w-full text-center text-sm text-zinc-400">
-          The viewer is Kevin. His word is 'YELLOW', he is trying to guess Bob's
-          word, and he has guessed Staurt's word.
+          The viewer is Kevin
         </div>
       </div>
-
       <p>
-        Everyone takes turns guessing letters, but everyone can guess any of the
+        In this example, the viewer, Kevin's secret word is <code class="bg-zinc-100 rounded p-1">YELLOW</code>. He is trying to guess
+        Bob's word, and he has guessed Staurt's word, which is <code class="bg-zinc-100 rounded p-1">ILLUMINATION</code>. Notice that the
+        letter <code class="bg-zinc-100 rounded p-1">U</code>
+        is not guessed yet. It is shown in Staurt's word because
+        Kevin has guessed it correctly. The letter may or may not be present in
+        Bob's word.
+      </p>
+      <p>
+        In the game, everyone takes turns guessing letters, but everyone can guess any of the
         others' secret words at any time. Your goal is to guess all the secret
         words faster than anyone else! Good luck!
       </p>
--- lib/playground_web/live/room_live.ex
+++ lib/playground_web/live/room_live.ex
@@ -12,7 +12,8 @@ defmodule PlaygroundWeb.RoomLive do
      |> assign(:code_from_session, session["room_code"])
      |> assign(:player_id, session["user_id"])
      |> assign(:player_name, session["user_name"])
-     |> assign(:is_info_modal_open, false)}
+     |> assign(:is_info_modal_open, false)
+     |> assign(:info_modal_show_how_to_play, false)}
   end
 
   @impl Phoenix.LiveView
@@ -165,12 +166,25 @@ defmodule PlaygroundWeb.RoomLive do
      |> assign(:is_info_modal_open, false)}
   end
 
+  def handle_event(
+        "toggle-how-to-play",
+        _params,
+        socket
+      ) do
+    state = socket.assigns[:info_modal_show_how_to_play]
+
+    {:noreply, assign(socket, :info_modal_show_how_to_play, !state)}
+  end
+
   def handle_event("open_info_modal", _params, socket) do
     {:noreply, assign(socket, is_info_modal_open: true)}
   end
 
   def handle_event("close_info_modal", _params, socket) do
-    {:noreply, assign(socket, is_info_modal_open: false)}
+    {:noreply,
+     socket
+     |> assign(is_info_modal_open: false)
+     |> assign(info_modal_show_how_to_play: false)}
   end
 
   def players_count(room) do
