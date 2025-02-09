964495eaffa504de02b159e51964eb3507d19427
Ethan <43922849+ethanppl@users.noreply.github.com>
2025-02-09 14:41:48 +0800

    Set endpoint config URL

--- config/prod.exs
+++ config/prod.exs
@@ -6,6 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :playground, PlaygroundWeb.Endpoint,
+  url: [host: "playground.ethanppl.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
